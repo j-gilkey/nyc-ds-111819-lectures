f6071ddfb709a23e017ec08e14bb61ed9d9bdee4
“jackson_gilkey” <jackson.gilkey@gmail.com>
2019-11-22 16:23:19 -0500

    final calculator

--- Mod_1/Calculator_class/descriptive.py
+++ Mod_1/Calculator_class/descriptive.py
@@ -11,34 +11,36 @@ import math
 class Calculator:
 
     def __init__(self, data):
-        self.data = data
-        self.length = len(data)
-        self.mean = self._calc_mean(data)
-        self.median = self._calc_median(data)
-        self.variance = self._calc_variance(data)
-        self.stand_dev = self._calc_std(data)
+        self.set_data(data)
+
+    def set_data(self, data):
+        self.data = sorted(data)
+        self.length = len(self.data)
+        self.mean = self._calc_mean()
+        self.median = self._calc_median()
+        self.variance = self._calc_variance()
+        self.stand_dev = self._calc_std()
 
     def add_data(self, new_data):
-        self.data = self.data.extend(new_data)
-        self.__init__(self, self.data)
+        self.data.extend(new_data)
+        self.set_data(self.data)
 
     def remove_data(self, bad_data):
         self.data = [elem for elem in self.data if elem not in bad_data]
-        self.__init__(self, self.data)
+        self.set_data(self.data)
 
-    def _calc_mean(self, data):
-        return sum(data)/len(data)
+    def _calc_mean(self):
+        return sum(self.data)/len(self.data)
 
-    def _calc_median(self, data):
-        sorted_data = data.sort()
-        middle_point = int(len(data)/2)
-        if len(data)%2:
-            return data[middle_point]
+    def _calc_median(self):
+        middle_point = int(self.length/2)
+        if self.length%2:
+            return self.data[middle_point]
         else:
-            return (data[middle_point] + data[middle_point - 1 ])/2
+            return (self.data[middle_point] + self.data[middle_point - 1 ])/2
 
-    def _calc_variance(self, data):
-        return sum([(datum -  self._calc_mean(data))**2 for datum in data])/(len(data) - 1)
+    def _calc_variance(self):
+        return sum([(datum -  self.mean)**2 for datum in self.data])/(self.length - 1)
 
-    def _calc_std(self, data):
-        return self._calc_variance(data)**0.5
+    def _calc_std(self):
+        return self._calc_variance()**0.5
