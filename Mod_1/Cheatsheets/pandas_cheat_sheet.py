aa7d05610be0d029bd443e14df75d8f86daa1b2a
“jackson_gilkey” <jackson.gilkey@gmail.com>
2019-12-03 10:02:36 -0500

    week 3.2.1 notes

--- Mod_1/Cheatsheets/pandas_cheat_sheet.py
+++ Mod_1/Cheatsheets/pandas_cheat_sheet.py
@@ -9,8 +9,15 @@ labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
 df = pd.DataFrame(exam_data , index=labels)
 
-#EXPLORATION METHODS
+#COPYING A DATAFRAME
 
+"""
+#a deep copy creates an actual copy of the dataframe instead of another pointer
+df_copy = df.copy(deep = True)
+"""
+
+#EXPLORATION METHODS
+"""
 #first 5 rows
 print(df.head())
 
@@ -23,12 +30,24 @@ print(df.index)
 #see list of column names
 print(df.columns)
 
-#COPYING A DATAFRAME
+#see column data types
+print(df.dtypes)
 
+#see dataframe shape
+print(df.shape)
 """
-#a deep copy creates an actual copy of the dataframe instead of another pointer
-df_copy = df.copy(deep = True)
-"""
+
+#SELECTION with .iloc() and .loc()
+
+#select all rows and only the 3rd column
+print(df.iloc[:, 2])
+
+#select first 3 rows and all columns
+print(df.iloc[:3])
+
+#select all rows and only the column score
+print(df.loc[:, 'score'])
+print(df.score)
 
 #FILTERING
 """
@@ -36,16 +55,20 @@ df_copy = df.copy(deep = True)
 attempts_over_2 = df[df['attempts'] > 2]
 print(attempts_over_2)
 
+#filter based on multiple conditionals
+attempts_under_2_score_over_15 = df[(df['attempts'] < 2) & (df['score'] > 15)]
+print(attempts_under_2_score_over_15)
+
 #filtered on NaN values
 missing_score = df[df['score'].isna()]
 print(missing_score)
-
 """
 
 #DATA CLEANING
 """
 #replace values based on multiple conditionals
 #note that this alters the original table
+#ALSO note that you must map every value else they will defualt to NaN
 print(df)
 df['qualify'] = df['qualify'].map({'yes': True, 'no' : False})
 print(df)
