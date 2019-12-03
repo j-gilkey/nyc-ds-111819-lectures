7d3e90429da2974b70f5395c35dbbb4cfa9fa3e9
“jackson_gilkey” <jackson.gilkey@gmail.com>
2019-12-03 16:01:02 -0500

    week 3.2.2 notes

--- Mod_1/Cheatsheets/pandas_cheat_sheet.py
+++ Mod_1/Cheatsheets/pandas_cheat_sheet.py
@@ -4,7 +4,8 @@ import numpy as np
 exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
 'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
 'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
-'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes'],
+'dates': ['2019-05-07', '2019-07-13', '2019-11-23', '2019-04-20', '2019-06-08', '2019-10-20', '2019-10-14', '2019-07-17', '2019-02-23', '2019-09-13']}
 labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
 df = pd.DataFrame(exam_data , index=labels)
@@ -38,7 +39,7 @@ print(df.shape)
 """
 
 #SELECTION with .iloc() and .loc()
-
+"""
 #select all rows and only the 3rd column
 print(df.iloc[:, 2])
 
@@ -48,6 +49,19 @@ print(df.iloc[:3])
 #select all rows and only the column score
 print(df.loc[:, 'score'])
 print(df.score)
+
+#select rows 3 - 5 and only columns 'score' and 'attempts'
+print(df.iloc[3:6][['score', 'attempts']])
+"""
+
+#SELECTION TIPS AND TRICKS
+"""
+#get value counts for column name
+print(df['attempts'].value_counts())
+
+#Percentage distribution
+print(df['attempts'].value_counts(normalize = True))
+"""
 
 #FILTERING
 """
@@ -65,16 +79,37 @@ print(missing_score)
 """
 
 #DATA CLEANING
-"""
+
 #replace values based on multiple conditionals
-#note that this alters the original table
-#ALSO note that you must map every value else they will defualt to NaN
+#NOTE that you must map every value else they will defualt to NaN
+#print(df)
+#df['qualify'] = df['qualify'].map({'yes': True, 'no' : False})
+#print(df)
+
+#second and better way
 print(df)
-df['qualify'] = df['qualify'].map({'yes': True, 'no' : False})
+map_dict = {'yes': True, 'no' : False}
+df['qualify'] = df['qualify'].replace(map_dict)
 print(df)
 
-#remove rows based on NaN values
+#remove rows based on NaN values in ['score']
+#print(df)
+df = df.dropna(subset = ['score'])
+#print(df)
 
 #remove rows based on a filter
 
-"""
+#clean whitespace off column names
+df.columns = [column.strip() for column in df.columns]
+
+#change ['attempts'] to a float
+df['attempts'] = df['attempts'].astype(float)
+#print(df['attempts'].dtype)
+
+#and back again
+df['attempts'] = df['attempts'].astype(int)
+#print(df['attempts'].dtype)
+
+#convert ['dates'] from string to datetime
+df['dates'] = pd.to_datetime(df['dates'])
+#print(df['dates'].dtype)
